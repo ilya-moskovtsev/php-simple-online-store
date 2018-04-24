c3f7ff63f9ae3c427a697a43f2963412852a111f
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2018-04-24 22:20:44 +0300

    show user registration form

--- config/routes.php
+++ config/routes.php
@@ -12,6 +12,7 @@ return array(
     'catalog' => 'catalog/index',
     'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',
     'category/([0-9]+)' => 'catalog/category/$1',
+    'user/register' => 'user/register',
     '' => 'site/index',
     'news/([0-9]+)' => 'news/view/$1', // NewsController::actionView
     'news' => 'news/index', // NewsController::actionIndex
---
+++ views/user/register.php
@@ -0,0 +1,23 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4 padding-right">
+                <div class="signup-form">
+                    <h2>Регистрация на сайте</h2>
+                    <form action="#" method="post">
+                        <input type="text" placeholder="Имя"/>
+                        <input type="email" placeholder="E-mail"/>
+                        <input type="password" placeholder="Пароль"/>
+                        <button type="submit" class="btn btn-default">Регистрация</button>
+                    </form>
+                </div>
+                <br/>
+                <br/>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>

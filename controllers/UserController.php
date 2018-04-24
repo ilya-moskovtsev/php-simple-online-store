fb61be8b49d50c3be283cd25e52ee15955b32431
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2018-04-24 22:38:17 +0300

    show data received from user registration form in the same form

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,20 +4,14 @@ class UserController
 {
     public function actionRegister()
     {
+        $name = '';
+        $email = '';
+        $password = '';
+
         if (isset($_POST['submit'])) {
             $name = $_POST['name'];
             $email = $_POST['email'];
             $password = $_POST['password'];
-
-            if (isset($name)) {
-                echo '<br>$name: ' . $name;
-            }
-            if (isset($email)) {
-                echo '<br>$email: ' . $email;
-            }
-            if (isset($password)) {
-                echo '<br>$password: ' . $password;
-            }
         }
         require_once(ROOT . '/views/user/register.php');
 
